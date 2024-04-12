03dc7dc26bc72ca9db7180ec5f8b06df5bdb7dc7
Peter Steneteg <peter@steneteg.se>
2024-04-12 12:55:25 +0200

    Added favicon

--- src/main.cpp
+++ src/main.cpp
@@ -108,7 +108,9 @@ std::string exceptionToString() {
 
 namespace html {
 constexpr std::string_view pre = R"(
-<html><head><style>
+<html><head>
+    <link rel="icon" type="image/svg+xml" href="favicon.svg" />
+<style>
 dl {
   display: grid;
   grid-template-columns: max-content auto;
@@ -149,6 +151,11 @@ constexpr std::string_view script = R"(
   };
 </script>
 )";
+
+constexpr std::string_view favicon = R"(
+<svg xmlns="http://www.w3.org/2000/svg" width="256" height="256" viewBox="0 0 100 100"><rect width="100" height="100" rx="50" fill="#4a85a9"></rect><path d="M39.25 86.31L39.25 86.31Q37.81 86.31 35.78 85.73Q33.76 85.14 32.18 83.39Q30.61 81.64 30.61 78.30L30.61 78.30Q30.61 75.52 31.69 71.56Q32.77 67.59 34.39 62.73Q36.01 57.88 37.63 52.25Q39.25 46.63 40.33 40.55Q41.41 34.48 41.41 28.18L41.41 28.18Q41.41 25.02 40.95 22.59Q40.51 20.16 39.34 18.82Q38.17 17.47 35.92 17.47L35.92 17.47Q33.58 17.47 31.96 18.82Q30.34 20.16 29.30 22.33Q28.27 24.48 27.77 26.83Q27.28 29.16 27.28 31.23L27.28 31.23Q27.28 32.77 27.59 34.20Q27.91 35.65 28.63 37.09L28.63 37.09Q24.22 37.09 22.24 35.02Q20.26 32.95 20.26 29.88L20.26 29.88Q20.26 27.19 21.61 24.30Q22.96 21.43 25.34 19.04Q27.73 16.66 30.92 15.17Q34.12 13.69 37.72 13.69L37.72 13.69Q43.84 13.69 46.98 18.14Q50.14 22.59 50.14 30.34L50.14 30.34Q50.14 35.29 49.01 40.69Q47.89 46.09 46.27 51.53Q44.65 56.98 42.98 62.11Q41.31 67.23 40.19 71.60Q39.06 75.97 39.06 79.20L39.06 79.20Q39.06 80.64 39.47 81.45Q39.88 82.27 41.23 82.27L41.23 82.27Q43.84 82.27 47.12 79.83Q50.41 77.41 53.92 73.22Q57.42 69.03 60.75 63.73Q64.09 58.41 66.78 52.66Q69.48 46.89 71.06 41.23Q72.64 35.55 72.64 30.79L72.64 30.79Q72.64 26.02 70.92 23.50Q69.22 20.98 66.61 20.08L66.61 20.08Q67.86 17.38 69.71 16.16Q71.56 14.95 73.00 14.95L73.00 14.95Q75.42 14.95 77.59 18.01Q79.75 21.07 79.75 26.65L79.75 26.65Q79.75 31.51 77.90 37.76Q76.06 44.02 72.81 50.72Q69.58 57.42 65.48 63.77Q61.39 70.11 56.84 75.20Q52.30 80.28 47.80 83.30Q43.30 86.31 39.25 86.31Z" fill="#fff"></path></svg>
+)";
+
 }  // namespace html
 
 size_t missmatches(const auto& map1, const auto& map2) {
@@ -440,8 +447,8 @@ int main(int argc, char* argv[]) {
 
         res.set_content_provider(
             info->size, "application/zip",
-            [fstream, buff = std::vector<char>(1024)](
-                size_t offset, size_t length, httplib::DataSink& sink) mutable -> bool {
+            [fstream, buff = std::vector<char>(1024)](size_t offset, size_t length,
+                                                      httplib::DataSink& sink) mutable -> bool {
                 buff.resize(length);
                 fstream->seekg(offset);
                 fstream->read(buff.data(), length);
@@ -580,6 +587,10 @@ int main(int argc, char* argv[]) {
             }
             res.status = httplib::StatusCode::InternalServerError_500;
         });
+
+    server.Get("/favicon.svg", [&](const httplib::Request&, httplib::Response& res) {
+        res.set_content(std::string{html::favicon}, "image/svg+xml");
+    });
 
     server.Get("/stop", [&](const httplib::Request&, httplib::Response&) { server.stop(); });
 
